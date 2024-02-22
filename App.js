f3a9012fb506ede2f63e000250178458e2b15849
anasamir2101 <anasamir9462@gmail.com>
2024-02-23 00:03:14 +0500

    fifth

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import { View, StyleSheet } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  StatusBar,
+  Button,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 import Hello from './components/Hello';
+import { useState } from 'react';
 
 const App = () => {
+  const [isStatusbarVisible, setisStatusbarVisible] = useState(false);
+
   return (
     <View style={Styles.container}>
-      <Hello />
+      {/* <StatusBar
+        backgroundColor={'green'}
+        barStyle={'dark-content'}
+        hidden={isStatusbarVisible}
+      /> */}
+      {/* <Button
+        title='Visible'
+        onPress={() => setisStatusbarVisible(!isStatusbarVisible)}
+      /> */}
+      {/* <ActivityIndicator
+        size={'extra-large'}
+        color={'black'}
+        animating={false}
+      /> */}
+      <Button
+        title='Click Me'
+        onPress={() =>
+          Alert.alert('Hello World', 'Welcome To React Native', [
+            {
+              text: 'Ok',
+              onPress: () => console.warn('Ok is Pressed'),
+            },
+            {
+              text: 'Cancel',
+              onPress: () => console.warn('Cancel is Pressed'),
+            },
+          ])
+        }
+      />
     </View>
   );
 };
@@ -13,6 +51,8 @@ const Styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'plum',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 export default App;
